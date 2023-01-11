e2e12c3619db8c287687b28468fc61655543e77d
jordiclive <jordiclive19@imperial.ac.uk>
2023-01-11 17:10:07 +0000

    a

--- open_assistant_training/convert.py
+++ open_assistant_training/convert.py
@@ -1,6 +1,6 @@
 from pytorch_lightning.utilities.deepspeed import convert_zero_checkpoint_to_fp32_state_dict
 
-save_path = '/fsx/home-jordiclive/open_assistant_training/results/20230111_1503/epoch=0-step=40.ckpt'
+save_path = '/fsx/home-jordiclive/open_assistant_training/results/20230111_1503/epoch=0-step=80.ckpt'
 # save_path = "/admin/home-jordiclive/LAION_projects/open_assistant_training/results/20230111_1553/epoch=0-step=24.ckpt"
 output_path = "new_model_large.pt"
 convert_zero_checkpoint_to_fp32_state_dict(save_path, output_path)
