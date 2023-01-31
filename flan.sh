2a2e51db88231e8632d0c71fedcf661448ef09fa
jordiclive <jordiclive19@imperial.ac.uk>
2023-01-31 21:23:05 +0000

    no message

--- flan.sh
+++ flan.sh
@@ -3,8 +3,9 @@
 #SBATCH --partition="g80n60"
 #SBATCH --job-name=flan
 #SBATCH --nodes 1
-#SBATCH --cpus-per-gpu=12
-#SBATCH --gpus=8
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=8
 #SBATCH --output=%x_%j.out
 
 #module load intelmpi
@@ -36,4 +37,5 @@ source /admin/home-jordiclive/jordan_flan/bin/activate
 cd /admin/home-jordiclive/LAION_projects/FLAN_code
 export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-srun --comment laion --gpus 8 python train.py
+srun --comment laion python train.py
+
