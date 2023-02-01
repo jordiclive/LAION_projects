c8c85cc7c9514f81aaf8063c84ca93eb7ae55b91
jordiclive <jordiclive19@imperial.ac.uk>
2023-02-01 10:28:07 +0000

    add see if batch size can be 1 higher

---
+++ flan_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account laion
+#SBATCH --partition="g80n60"
+#SBATCH --job-name=flan
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --output=%x_%j.out
+
+
+
+source /admin/home-jordiclive/jordan_flan/bin/activate
+cd /admin/home-jordiclive/LAION_projects/FLAN_code
+export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+srun python train.py --train_batch_size 3 --val_check_interval 0.15
