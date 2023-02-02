4dfe8cb2e7eac5e7c23791a2aa2a1b0201fe264a
jordiclive <jordiclive19@imperial.ac.uk>
2023-02-02 19:16:23 +0000

    a

---
+++ convert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account laion
+#SBATCH --partition="g80n140"
+#SBATCH --job-name=flan
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=10
+#SBATCH --output=%x_%j.out
+
+
+
+source /admin/home-jordiclive/jordan_flan/bin/activate
+cd /admin/home-jordiclive/LAION_projects/FLAN_code
+export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+srun python convert_working.py
