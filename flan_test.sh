d3f509b351ea8f657b884cf9ab3fc49742547ccc
jordiclive <jordiclive19@imperial.ac.uk>
2023-02-02 19:28:54 +0000

    A

---
+++ flan_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account laion
+#SBATCH --partition="g80n140"
+#SBATCH --job-name=flan
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=10
+#SBATCH --output=%x_%j.out
+
+
+
+source /admin/home-jordiclive/jordan_flan/bin/activate
+cd /admin/home-jordiclive/LAION_projects/FLAN_code
+export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+srun python train.py --train_batch_size 4 --val_check_interval 0.12 --wb_name "bs4-retrain-more-prompts-13B" --gradient_accumulation_steps 2 --learning_rate 0.0003 --data_path '/admin/home-jordiclive/LAION_projects/summarization_data_prep/processing/final_test_dataset'
