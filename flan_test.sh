9f8e4c4b22eb2def4f58688499f397b4bcb4f071
jordiclive <jordiclive19@imperial.ac.uk>
2023-02-03 10:46:23 +0000

    update save more regularly

--- flan_test.sh
+++ flan_test.sh
@@ -13,4 +13,4 @@ source /admin/home-jordiclive/jordan_flan/bin/activate
 cd /admin/home-jordiclive/LAION_projects/FLAN_code
 export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-srun python train.py --train_batch_size 4 --val_check_interval 0.12 --wb_name "bs4-retrain-more-prompts-13B" --gradient_accumulation_steps 2 --learning_rate 0.0003 --data_path '/admin/home-jordiclive/LAION_projects/summarization_data_prep/processing/final_test_dataset'
+srun python train.py --train_batch_size 4 --val_check_interval 0.05 --wb_name "bs4-retrain-more-prompts-13B" --gradient_accumulation_steps 2 --learning_rate 0.0003 --data_path '/admin/home-jordiclive/LAION_projects/summarization_data_prep/processing/final_test_dataset'
