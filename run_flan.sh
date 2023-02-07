79216ad843ac351a1a6d8109b63471301b3b79df
jordiclive <jordiclive19@imperial.ac.uk>
2023-02-07 13:25:19 +0000

    a

--- run_flan.sh
+++ run_flan.sh
@@ -13,4 +13,4 @@ source /admin/home-jordiclive/jordan_flan/bin/activate
 cd /admin/home-jordiclive/LAION_projects/FLAN_code
 export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-srun python train.py --train_batch_size 4 --val_check_interval 0.12 --wb_name "full-lower-lr-1" --gradient_accumulation_steps 2 --learning_rate 0.0001 --data_path '/admin/home-jordiclive/LAION_projects/summarization_data_prep/processing/final_dataset'
+srun python train.py --train_batch_size 4 --val_check_interval 0.1 --wb_name "full-lower-lr-1" --gradient_accumulation_steps 2 --learning_rate 0.0001 --data_path '/admin/home-jordiclive/LAION_projects/summarization_data_prep/processing/final_dataset'
