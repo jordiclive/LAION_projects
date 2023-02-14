a3bab5e8e36f36bacc982ceb78d119ed46cb0d9d
jordiclive <jordiclive19@imperial.ac.uk>
2023-02-14 22:38:55 +0000

    a

--- GPTNEO/chip_non_grad.sh
+++ GPTNEO/chip_non_grad.sh
@@ -13,4 +13,4 @@ source /admin/home-jordiclive/jordan_deep/bin/activate
 cd /admin/home-jordiclive/LAION_projects/GPTNEO
 export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-srun python train.py --precision: "16" --grad_checkpoint False --lr_flat False --warmup_steps 1000 --train_batch_size 8 --eval_batch_size 8 --wb_name "without"
+srun python train.py --precision "16" --grad_checkpoint False --lr_flat False --warmup_steps 1000 --train_batch_size 8 --eval_batch_size 8 --wb_name "without"
